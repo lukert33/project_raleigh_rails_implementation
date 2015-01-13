4f45f6ce76185f75d21b5dc4b6e3871704be42a8
Luke Thomas <Lukert33@gmail.com>
2015-01-13 18:48:40 -0500

    Add new User creation with support for strong params.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,5 +8,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def new
+    @current_user = User.new
+  end
+
+  def create
+    @current_user = User.new(user_params)
+    if @current_user.save
+      session[:user_id] = @current_user.id
+      redirect_to user_path(@current_user)
+    else
+      render :new
+    end
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :username, :password, :password_confirmation)
+  end
 
 end
